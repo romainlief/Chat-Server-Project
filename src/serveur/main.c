0437775891d6690677cecd38d2255fe7d661f3c3
romainlief <romain.liefferinckx@ulb.be>
2024-12-04 20:37:16 +0100

    ajout include dans serveur

--- src/serveur/main.c
+++ src/serveur/main.c
@@ -1,3 +1,18 @@
+#include <signal.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+
+#define MAX_CLIENTS 1000
+#define MAX_LEN_MESSAGE 1024
+#define PORT_PAR_DEFAULT 1234
+
+
 int main(void) {
    return 0;
 }
