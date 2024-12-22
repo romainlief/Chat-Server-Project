b435adcffc929169010563926f2712716df461a5
romainlief <romain.liefferinckx@ulb.be>
2024-12-22 14:19:25 +0100

    factorisation client

--- src/chat/main.c
+++ src/chat/main.c
@@ -8,6 +8,9 @@
 
 extern liste_t memoire;
 extern OptionsProgramme options;
+
+void process_message(char *message, size_t size_mess, int sock, OptionsProgramme options, Arguments argument);
+
 int main(int argc, char *argv[])
 {
    
@@ -78,70 +81,12 @@ int main(int argc, char *argv[])
    pthread_t second_thread;
    pthread_create(&second_thread, NULL, &readerThread, &argument); //creation thread lecture
 
-   char *token;
-
    while ((code = getline(&message, &size_mess, stdin))) {
       if (code == -1)
       {
          break; // tant que stdin est ouvert on peut écrire
       }
-
-      int spacecounter = 0;
-      int idx = 0;
-
-      while (message[idx] == ' ')
-      {
-         spacecounter++;
-         idx++;
-      }
-
-      char *verificateur = strdup(message);  // verification de message valide: 2 mots min
-      token = strtok(verificateur, " ");
-      token = strtok(NULL, " ");
-      if (token == NULL || strcmp(token, "\n") == 0 || spacecounter > 0)
-      {
-         if (code == -1)
-         {
-            free(verificateur);
-            break;
-         }
-         free(verificateur);
-         printf("Message non valide\n");
-         continue;
-      }
-
-      char temp[size_mess];
-      memcpy(temp, message, size_mess);
-
-      if (!options.modeBot)
-      {
-         printf("[\x1B[4m%s\x1B[0m] %s", argument.utilisateur, temp);
-
-         fflush(stdout);
-      }
-
-      if (options.affichageManuel)
-      {
-         char *msg = popStr(&memoire);
-         while (msg != NULL) // vidage mémoire 
-         {
-            if(options.modeBot){ // ecriture message sans soulignement
-            printf("%s", msg);
-            }
-            else{    // ecriture message avec soulignement
-               char *separators = "[]";
-               char *tok = strtok(msg, separators);
-               printf("[\x1B[4m%s\x1B[0m]", tok);
-               tok = strtok(NULL, "");
-               printf("%s", tok);
-            }
-            free(msg);
-            msg = popStr(&memoire);
-         }
-      }
-
-      write(sock, temp, sizeof(temp));
-      free(verificateur);
+      process_message(message, size_mess, sock, options, argument);
    }
 
    free(message);
@@ -149,4 +94,52 @@ int main(int argc, char *argv[])
    close(sock);
    pthread_join(second_thread, NULL);
    return 0;
+}
+
+
+void process_message(char *message, size_t size_mess, int sock, OptionsProgramme options, Arguments argument) {
+    int spacecounter = 0;
+    int idx = 0;
+
+    while (message[idx] == ' ') {
+        spacecounter++;
+        idx++;
+    }
+
+    char *verificateur = strdup(message);  // verification de message valide: 2 mots min
+    char *token = strtok(verificateur, " ");
+    token = strtok(NULL, " ");
+    if (token == NULL || strcmp(token, "\n") == 0 || spacecounter > 0) {
+        free(verificateur);
+        printf("Message non valide\n");
+        return;
+    }
+
+    char temp[size_mess];
+    memcpy(temp, message, size_mess);
+
+    if (!options.modeBot) {
+        printf("[\x1B[4m%s\x1B[0m] %s", argument.utilisateur, temp);
+        fflush(stdout);
+    }
+
+    if (options.affichageManuel) {
+        char *msg = popStr(&memoire);
+        while (msg != NULL) { // vidage mémoire 
+            if (options.modeBot) { // ecriture message sans soulignement
+                printf("%s", msg);
+            } else {    // ecriture message avec soulignement
+                char *separators = "[]";
+                char *tok = strtok(msg, separators);
+                printf("[\x1B[4m%s\x1B[0m]", tok);
+                tok = strtok(NULL, "");
+                printf("%s", tok);
+            }
+            free(msg);
+            msg = popStr(&memoire);
+        }
+    }
+
+    write(sock, temp, sizeof(temp));
+    free(verificateur);
 }
